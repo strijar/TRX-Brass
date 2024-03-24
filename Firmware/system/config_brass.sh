31be2df256ff130aa071a95b12ad3cdca261931e
Belousov Oleg <belousov.oleg@gmail.com>
2024-03-24 19:19:53 +0300

    Update buildroot

--- Firmware/system/config_brass.sh
+++ Firmware/system/config_brass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-BR_RELEASE="buildroot-2023.08.2"
+BR_RELEASE="buildroot-2024.02"
 
 if [ ! -e ${BR_RELEASE}.tar.gz ]; then
   wget https://buildroot.org/downloads/${BR_RELEASE}.tar.gz
