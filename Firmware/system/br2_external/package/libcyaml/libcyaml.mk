a60118143ceec82fa141ebd8ac8cfb8c22abe452
Belousov Oleg <belousov.oleg@gmail.com>
2025-01-06 19:03:59 +0300

    Switch settings to yaml files

---
+++ Firmware/system/br2_external/package/libcyaml/libcyaml.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# libcyaml
+#
+################################################################################
+
+LIBCYAML_VERSION = 1.4.2
+LIBCYAML_SITE = $(call github,tlsa,libcyaml,v$(LIBCYAML_VERSION))
+LIBCYAML_DEPENDENCIES = libyaml
+LIBCYAML_INSTALL_STAGING = YES
+
+define LIBCYAML_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) \
+		CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
+		WARNINGS="" -C $(@D) all
+endef
+
+define LIBCYAML_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(STAGING_DIR)" -C $(@D) install
+endef
+
+define LIBCYAML_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
+endef
+
+$(eval $(generic-package))
