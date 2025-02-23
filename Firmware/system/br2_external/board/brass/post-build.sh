5ad53f926a606a022a6548b3646695d73c25f87a
Belousov Oleg <belousov.oleg@gmail.com>
2025-02-23 20:59:47 +0300

    Support zynq7010

--- Firmware/system/br2_external/board/brass/post-build.sh
+++ Firmware/system/br2_external/board/brass/post-build.sh
@@ -6,4 +6,4 @@
 BOARD_DIR="${BR2_EXTERNAL_BRASS_PATH}/board/brass"
 
 install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${BINARIES_DIR}/extlinux.conf"
-install -m 0644 -D "${BOARD_DIR}/fpga/system.bit" "${BINARIES_DIR}/system.bit"
+install -m 0644 -D "${BOARD_DIR}/$2/system.bit" "${BINARIES_DIR}/system.bit"
